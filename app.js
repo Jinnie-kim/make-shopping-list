dcd65f8bfe636dd2a68422dc2d27619debbbc90f
HeeJin <hjk7639@gmail.com>
2022-01-13 13:30:49 +0900

    JS refactoring
    솔루션 강의에서 배운 방식으로 리팩토링

--- app.js
+++ app.js
@@ -1,36 +1,48 @@
-'use strict';
+'use strict'
 
-const form = document.querySelector('#shopping-list-form');
-const inputText = document.querySelector('.shopping-list-input');
-const listBoard = document.querySelector('.shopping-list-board');
-const  listParent = document.querySelector('.shopping-list-item');
-const button = document.querySelector('button');
+const shoppingBoard = document.querySelector('.shopping-board');
+const addBtn = document.querySelector('.shopping-add-btn');
+const input = document.querySelector('.shopping-input');
 
+function onAdd () {
+    const listItem = input.value;
 
-button.addEventListener('click', (e) => {
-    e.preventDefault();
-    if(inputText.value.length == 0) {
-        alert('Please write items')
-    } else if(inputText.value.length != 0) {
-    makeList();
-    }
-})
+    const list = createLists(listItem);
 
-function makeList() {
-    const li = document.createElement('li');
-    li.textContent = inputText.value;
-    const deleteButton = document.createElement('button');
-    deleteButton.setAttribute('class', 'far fa-check-square');
-    deleteButton.addEventListener('click', () => {
-        li.parentNode.removeChild(li);
-    })  
-    li.appendChild(deleteButton);
-    listParent.appendChild(li);  
-    inputText.value = '';    
+    shoppingBoard.appendChild(list);
+
+    input.value = '';
+    input.focus();
+
 }
 
-// function deleteList() {
-//     window.addEventListener('load', () => {
-//         li.parentNode.removeChild(li);
-//     })
-// }
+function createLists (listItem) {
+
+    const list = document.createElement('div');
+    list.setAttribute('class', 'shopping-list');
+    
+    const listRow = document.createElement('li');
+    listRow.setAttribute('class', 'shopping-list-row');
+
+    const text = document.createElement('span');
+    text.setAttribute('class', 'shopping-list');
+    text.innerText = listItem;
+
+    const deleteBtn = document.createElement('button');
+    deleteBtn.setAttribute('class', 'shopping-delete-btn');
+    deleteBtn.innerHTML = '<i class="fas fa-trash"></i>';
+    deleteBtn.addEventListener('click', () => {
+        list.removeChild(listRow);
+    })
+
+    listRow.appendChild(text);
+    listRow.appendChild(deleteBtn);
+
+    list.appendChild(listRow);
+
+    return list;
+}
+
+addBtn.addEventListener('click', () => {
+    onAdd();
+})
